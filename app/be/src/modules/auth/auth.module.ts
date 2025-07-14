0a411790f2362581e2f8e87b0b27818668109ec0
albarp <alberto.arpini@gmail.com>
2025-07-14 13:23:35 +0000

    aut moved jwt secret to env file. Implemented guard

--- app/be/src/modules/auth/auth.module.ts
+++ app/be/src/modules/auth/auth.module.ts
@@ -3,15 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('AUTH_JWT_SECRET'),
+          signOptions: { expiresIn: '60s' },
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
